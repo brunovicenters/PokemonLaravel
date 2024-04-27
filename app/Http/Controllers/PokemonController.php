c58be20d68ac3cc98d600a23b290103f4344b804
bruno <brunovicente.dev@gmail.com>
2024-04-26 21:22:31 -0300

    Added index, create and store

---
+++ app/Http/Controllers/PokemonController.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Pokemon;
+use Illuminate\Http\Request;
+
+class PokemonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('pokemon.index', ['pokemons' => Pokemon::all()]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('pokemon.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $pokemon = [
+            'name' => $request->name,
+            'level' => $request->level,
+            'number' => $request->number,
+            'nickname' => $request->nickname,
+            'region' => $request->region,
+            'created_at' => now(),
+        ];
+
+        Pokemon::create($pokemon);
+
+        return redirect()->route('pokemon.index');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Pokemon $pokemon)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Pokemon $pokemon)
+    {
+        //
+    }
+}
