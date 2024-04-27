66fbf719aee208f1cdb402d0f150e9dadbabdfdf
bruno <brunovicente.dev@gmail.com>
2024-04-26 21:07:58 -0300

    Added Pokemon Controller

--- routes/web.php
+++ routes/web.php
@@ -2,10 +2,13 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PokemonController;
 
 Route::get('/', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::resource('pokemon', PokemonController::class);
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
