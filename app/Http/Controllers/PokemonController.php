89b1f89c3620544a2796a42c5c1cb20ae5906ddb
bruno <brunovicente.dev@gmail.com>
2024-04-26 21:35:09 -0300

    Added destroy

--- app/Http/Controllers/PokemonController.php
+++ app/Http/Controllers/PokemonController.php
@@ -47,7 +47,7 @@ class PokemonController extends Controller
      */
     public function show(Pokemon $pokemon)
     {
-        //
+        return view('pokemon.show', ['pokemon' => $pokemon]);
     }
 
     /**
@@ -55,7 +55,7 @@ class PokemonController extends Controller
      */
     public function edit(Pokemon $pokemon)
     {
-        //
+        return view('pokemon.edit', ['pokemon' => $pokemon]);
     }
 
     /**
@@ -71,6 +71,8 @@ class PokemonController extends Controller
      */
     public function destroy(Pokemon $pokemon)
     {
-        //
+        $pokemon->delete();
+
+        return redirect()->route('pokemon.index');
     }
 }
